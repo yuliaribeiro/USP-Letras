e74b53ca3ff32efbf4ae66b3307f3077098599ab
Ju Ribeiro <juribeiro.ru@gmail.com>
2022-11-16 19:13:12 -0300

    feat: Add icons from FontAwesome as components

--- src/icons/fontAwesomeIcons.tsx
+++ src/icons/fontAwesomeIcons.tsx
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faHouse, faUserGroup, faBookOpen, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-export const Book = () => <FontAwesomeIcon icon={faBookOpen} />;
-export const Email = () => <FontAwesomeIcon icon={faEnvelope} />;
-export const Home = () => <FontAwesomeIcon icon={faHouse} />;
+export const Book = () => <FontAwesomeIcon icon={faBookOpen} className="menuDrawer" />;
+export const Email = () => <FontAwesomeIcon icon={faEnvelope} className="menuDrawer" />;
+export const Home = () => <FontAwesomeIcon icon={faHouse} className="menuDrawer" />;
 export const Menu = () => <FontAwesomeIcon icon={faBars} />;
-export const Users = () => <FontAwesomeIcon icon={faUserGroup} />;
+export const Users = () => <FontAwesomeIcon icon={faUserGroup} className="menuDrawer" />;
