23dd5dd009b555b663ee99c2a098bfd6ce188a73
Ju Ribeiro <juribeiro.ru@gmail.com>
2022-11-20 22:04:29 -0300

    feat: Apply carousel to the project

--- src/components/Writer.tsx
+++ src/components/Writer.tsx
@@ -3,14 +3,16 @@ import { Avatar, Grid, Typography } from "@mui/material";
 export function Writer() {
 	return (
 		<>
-			<Grid container sx={{ width: "60%", marginLeft: "auto", marginRight: "auto", paddingBottom: 10 }}>
+			<Grid container sx={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
 				<Grid item>
 					<Avatar
 						alt="research name" //TODO Change name IAW H. info
 						src="https://avatars.githubusercontent.com/u/89787597?v=4" //TODO Change pic
 						sx={{
 							marginRight: 2,
-							boxShadow: 5,
+							marginTop: 2,
+							marginBottom: 2,
+							boxShadow: 6,
 							width: { lg: 200, md: 150, sm: 100, xs: 0 },
 							height: { lg: 200, md: 150, sm: 100, xs: 0 },
 						}}
