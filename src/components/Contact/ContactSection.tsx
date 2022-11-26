14c3b83814b33bac76be8eb0f9cb3642ff24f9aa
Ju Ribeiro <juribeiro.ru@gmail.com>
2022-11-26 20:02:03 -0300

    feat: Create contact page

---
+++ src/components/Contact/ContactSection.tsx
@@ -0,0 +1,93 @@
+import { Button, Card, CardContent, Grid, TextField } from "@mui/material";
+
+import { theme } from "../../styles/reactColorThemes";
+import { ThemeProvider } from "@mui/material/styles";
+
+const colorThemesForComponents = theme;
+
+const textFieldList = [
+	{
+		label: "first name",
+		type: "text",
+		required: true,
+		md: 6,
+	},
+	{
+		label: "last name",
+		type: "text",
+		required: false,
+		md: 6,
+	},
+	{
+		label: "email",
+		type: "email",
+		required: true,
+		md: 12,
+	},
+	{
+		label: "subject",
+		type: "text",
+		required: true,
+		md: 12,
+	},
+];
+
+export function ContactSection() {
+	return (
+		<ThemeProvider theme={colorThemesForComponents}>
+			<Card sx={{ maxWidth: 500, maxHeight: 530, bgcolor: "transparent" }}>
+				<CardContent>
+					<form>
+						<Grid container spacing={2}>
+							{/* @ts-ignore */}
+							{Object(textFieldList).map((item, key) => {
+								return (
+									<Grid key={key} item md={item.md} xs={12}>
+										<TextField
+											type={item.type}
+											label={item.label}
+											variant="outlined"
+											placeholder={item.label}
+											fullWidth
+											required={item.required}
+										/>
+									</Grid>
+								);
+							})}
+							<Grid item xs={12}>
+								<TextField
+									label="message"
+									variant="outlined"
+									placeholder="message"
+									multiline
+									rows={5}
+									fullWidth
+									required
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<Button
+									type="submit"
+									color="error"
+									size="large"
+									sx={{
+										borderRadius: 5,
+										fontFamily: "montserrat",
+										fontWeight: "bold",
+										":hover": {
+											bgcolor: "lightRed.main",
+										},
+									}}
+									variant="contained"
+									fullWidth
+								>
+									Submit
+								</Button>
+							</Grid>
+						</Grid>
+					</form>
+				</CardContent>
+			</Card>
+		</ThemeProvider>
+	);
+}
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,3 +1,21 @@
+import { Grid } from "@mui/material";
+import { ContactSection } from "../../components/Contact/ContactSection";
+import { NavBarSection } from "../../components/NavBar/NavBarSection";
+
 export default function Contact() {
-	return <h1>Contact page</h1>;
+	return (
+		<div className="dimentionToScreen">
+			<NavBarSection />
+			<Grid container flexDirection="column" alignItems="center">
+				<Grid item sx={{ marginBottom: 5 }}>
+					<h1 className="landingPageTitle">
+						Let's get in <span className="text-blue-400">touch</span>!
+					</h1>
+				</Grid>
+				<Grid item>
+					<ContactSection />
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
