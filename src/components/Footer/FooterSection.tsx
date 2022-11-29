9a6e61bd7c043e94a803d9375a0c004c428b444d
Ju Ribeiro <juribeiro.ru@gmail.com>
2022-11-29 02:12:17 -0300

    feat: Add functionalities to footer buttons

--- src/components/Footer/FooterSection.tsx
+++ src/components/Footer/FooterSection.tsx
@@ -2,18 +2,32 @@ import { IconButton, Stack } from "@mui/material";
 import { Email, GitHub, LinkedIn } from "../icons/fontAwesomeIcons";
 
 export function FooterSection() {
-	const listIcons = {
-		github: <GitHub />,
-		linkedin: <LinkedIn />,
-		email: <Email />,
+	const listIcons = [
+		{
+			icon: <GitHub />,
+			url: "https://github.com/yuliaribeiro",
+		},
+		{
+			icon: <LinkedIn />,
+			url: "https://www.linkedin.com/in/juliana-ribeiro-039883220/",
+		},
+		{
+			icon: <Email />,
+			url: "mailto:lutchenikova@gmail.com",
+		},
+	];
+
+	const handleClick = (url: string) => {
+		window.open(url, "_blank");
 	};
+
 	return (
 		<div className="boxFooter">
 			<Stack direction="row" alignItems="center" justifyContent="center" spacing={2}>
 				<p className="py-2"> Did you like this website? Let's connect!</p>
-				{Object.values(listIcons).map((icon, index) => (
-					<IconButton key={index} size="small">
-						{icon}
+				{Object.values(listIcons).map((item, index) => (
+					<IconButton key={index} size="small" onClick={() => handleClick(item.url)}>
+						{item.icon}
 					</IconButton>
 				))}
 			</Stack>
