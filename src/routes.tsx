e336d50edcf8bbd6ceccf39085c02f12c55aad41
Ju Ribeiro <juribeiro.ru@gmail.com>
2022-11-28 23:06:14 -0300

    feat: Apply errorpage to router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { ArticlesPage } from "./pages/ArticlesPage/ArticlesPage";
 import Contact from "./pages/Contact/Contact";
+import { ErrorPage } from "./pages/Error/ErrorPage";
 import { Home } from "./pages/Home/Home";
 
 export const routes = createBrowserRouter([
 	{
 		path: "/",
 		element: <Home />,
+		errorElement: <ErrorPage />,
 	},
 	{
 		path: "/articles",
